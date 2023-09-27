f9928a8c3c59520250f92eb618fc22dcb25ddc35
Meiloorun <hishaam.k@live.co.uk>
2023-09-27 21:37:43 +0100

    start checks occupied spaces

--- Battleship/Program.fs
+++ Battleship/Program.fs
@@ -23,8 +23,8 @@ for x = 1 to 10 do
 
 printfn "The Battleship Board: %A" board
 
-let mutable battleship_x = System.Random().Next(1,11)
-let mutable battleship_y = System.Random().Next(1,11)
+let mutable battleship_x = System.Random().Next(1,10)
+let mutable battleship_y = System.Random().Next(1,5)
 let battleship_orientation = System.Random().Next(1, 5) //1 - North, 2 - East, 3 - South, 4 - West
 
 if battleship_orientation = 1 && battleship_y - 5 < 1 then
@@ -62,8 +62,8 @@ for i = 0 to 4 do
     elif battleship_orientation = 3 then battleship_x <- battleship_x - 1
     elif battleship_orientation = 4 then battleship_y <- battleship_y + 1
 
-let mutable destroyer1_x = System.Random().Next(1,11)
-let mutable destroyer1_y = System.Random().Next(1,11)
+let mutable destroyer1_x = System.Random().Next(1,10)
+let mutable destroyer1_y = System.Random().Next(1,5)
 let destroyer1_orientation = System.Random().Next(1, 5) //1 - North, 2 - East, 3 - South, 4 - West
 
 if destroyer1_orientation = 1 && destroyer1_y - 4 < 1 then
@@ -101,11 +101,51 @@ for i = 0 to 3 do
     elif destroyer1_orientation = 3 then destroyer1_x <- destroyer1_x - 1
     elif destroyer1_orientation = 4 then destroyer1_y <- destroyer1_y + 1
     
-let destroyer2 =
-    Map.empty
-    |> Map.add "Point 1" true
-    |> Map.add "Point 2" true
-    |> Map.add "Point 3" true
-    |> Map.add "Point 4" true
+let valid = false
+let mutable destroyer2_x= 0
+let mutable destroyer2_y= 0
+let mutable destroyer2_orientation = 0
 
+while valid = false do
+    destroyer2_x <- System.Random().Next(1,10)
+    destroyer2_y <- System.Random().Next(1,5)
+    destroyer2_orientation <- System.Random().Next(1, 5) //1 - North, 2 - East, 3 - South, 4 - West
+    let destroyer2_coord = sprintf "%s%d" letter destroyer2_y
+
+    if destroyer2_orientation = 1 && destroyer2_y - 4 < 1 then
+        while destroyer2_y - 4 < 1 do
+            //if 
+            destroyer2_y <- destroyer2_y + 1
+
+    if destroyer2_orientation = 4 && 10 - destroyer2_y > 6 then
+        while 10 - destroyer2_y > 6 do destroyer2_y <- destroyer2_y - 1
+
+    if destroyer2_orientation = 2 && 10 - destroyer2_x > 6 then
+        while 10 - destroyer2_x > 6 do destroyer2_x <- destroyer2_x - 1
+
+    if destroyer2_orientation = 3 && destroyer2_x - 4 < 1 then
+        while destroyer2_x - 4 < 1 do destroyer2_x <- destroyer2_x + 1
+
+for i = 0 to 3 do
+    let letter = 
+            if destroyer2_x = 1 then "A"
+            elif destroyer2_x = 2 then "B"
+            elif destroyer2_x = 3 then "C"
+            elif destroyer2_x = 4 then "D"
+            elif destroyer2_x = 5 then "E"
+            elif destroyer2_x = 6 then "F"
+            elif destroyer2_x = 7 then "G"
+            elif destroyer2_x = 8 then "H"
+            elif destroyer2_x = 9 then "I"
+            elif destroyer2_x = 10 then "J"
+            else invalidArg "letter" (sprintf "Invalid index %d used." destroyer2_x)
+
+    let destroyer2_coord = sprintf "%s%d" letter destroyer2_y
+    board.[destroyer2_coord] <- true
+    let temp = board.[destroyer2_coord]
+    printfn "The value at %s is: %b" destroyer2_coord temp 
+    if destroyer2_orientation = 1 then destroyer2_y <- destroyer2_y - 1
+    elif destroyer2_orientation = 2 then destroyer2_x <- destroyer2_x + 1
+    elif destroyer2_orientation = 3 then destroyer2_x <- destroyer2_x - 1
+    elif destroyer2_orientation = 4 then destroyer2_y <- destroyer2_y + 1
 
